59e889a29d4485086041fc60cb213418c7de780e
nikkacabaniaaa <122726783+scorkie@users.noreply.github.com>
2024-05-31 16:53:22 +0800

    Problem1.java

--- Problem1.java
+++ Problem1.java
@@ -9,33 +9,33 @@ import java.util.*;
 
 public class Problem1 {
     
-    private static class Graph {
-        private final int vertices;
-        private final List<List<Integer>> adjList;
+    private static class GraphCabania {
+        private final int verticesCabania;
+        private final List<List<Integer>> adjListCabania;
         
-        public Graph(int vertices) {
-            this.vertices = vertices;
-            adjList = new ArrayList<>(vertices);
+        public GraphCabania(int vertices) {
+            this.verticesCabania = vertices;
+            adjListCabania = new ArrayList<>(vertices);
             for (int i = 0; i < vertices; i++) {
-                adjList.add(new ArrayList<>());
+                adjListCabania.add(new ArrayList<>());
             }
         }
         
-        public void addEdge(int src, int dest) {
-            adjList.get(src).add(dest);
-            adjList.get(dest).add(src);
+        public void addEdge(int srcCabania, int destCabania) {
+            adjListCabania.get(srcCabania).add(destCabania);
+            adjListCabania.get(destCabania).add(srcCabania);
         }
         
-        public int getVertices() {
-            return vertices;
+        public int getVerticesCabania() {
+            return verticesCabania;
         }
         
-        public List<Integer> getAdjList(int vertex) {
-            return adjList.get(vertex);
+        public List<Integer> getAdjList(int vertexCabania) {
+            return adjListCabania.get(vertexCabania);
         }
     }
     
-    private static void dfs(Graph graph, int vertex, boolean[] visited) {
+    private static void dfs(GraphCabania graph, int vertex, boolean[] visited) {
         visited[vertex] = true;
         for (int neighbor : graph.getAdjList(vertex)) {
             if (!visited[neighbor]) {
@@ -44,10 +44,10 @@ public class Problem1 {
         }
     }
     
-    public static boolean isConnected(Graph graph) {
-        boolean[] visited = new boolean[graph.getVertices()];
-        dfs(graph, 0, visited);
-        for (boolean v : visited) {
+    public static boolean isConnected(GraphCabania graph) {
+        boolean[] visitedCabania = new boolean[graph.getVerticesCabania()];
+        dfs(graph, 0, visitedCabania);
+        for (boolean v : visitedCabania) {
             if (!v) {
                 return false;
             }
@@ -55,29 +55,29 @@ public class Problem1 {
         return true;
     }
     
-    public static int numberOfConnectedComponents(Graph graph) {
-        boolean[] visited = new boolean[graph.getVertices()];
-        int count = 0;
-        for (int i = 0; i < graph.getVertices(); i++) {
-            if (!visited[i]) {
-                dfs(graph, i, visited);
-                count++;
+    public static int numberOfConnectedComponents(GraphCabania graph) {
+        boolean[] visitedCabania = new boolean[graph.getVerticesCabania()];
+        int countCabania = 0;
+        for (int i = 0; i < graph.getVerticesCabania(); i++) {
+            if (!visitedCabania[i]) {
+                dfs(graph, i, visitedCabania);
+                countCabania++;
             }
         }
-        return count;
+        return countCabania;
     }
     
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Enter number of vertices:");
-        int vertices = scanner.nextInt();
+        int verticesCabania = scanner.nextInt();
         System.out.println("Enter number of edges:");
-        int edges = scanner.nextInt();
+        int edgesCabania = scanner.nextInt();
         
-        Graph graph = new Graph(vertices);
+        GraphCabania graph = new GraphCabania(verticesCabania);
         
         System.out.println("Enter the edges (source and destination):");
-        for (int i = 0; i < edges; i++) {
+        for (int i = 0; i < edgesCabania; i++) {
             int src = scanner.nextInt();
             int dest = scanner.nextInt();
             graph.addEdge(src, dest);
@@ -90,5 +90,6 @@ public class Problem1 {
             System.out.println("The graph is not connected.");
             System.out.println("Number of connected components: " + components);
         }
+        scanner.close();
     }
 }
